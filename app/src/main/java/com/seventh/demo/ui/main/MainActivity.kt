a697221ab5afd9cd6ba2dfd8d27788492fb670e4
seventh <gao_rui2011@163.com>
2022-05-17 23:29:41 +0800

    add MainActivity

---
+++ app/src/main/java/com/seventh/demo/ui/main/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.seventh.demo.ui.main
+
+import com.seventh.demo.base.BaseAppCompatActivity
+import com.seventh.demo.databinding.ActivityMainBinding
+
+class MainActivity: BaseAppCompatActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+
+    override fun initView() {
+
+    }
+
+    override fun initViewEvents() {
+
+    }
+
+    override fun initViewStates() {
+
+    }
+}
